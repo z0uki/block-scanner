bcac1291e71ff88036b84fc5fe153d2613393060
sicssss <sicdesson@gmail.com>
2024-03-09 10:48:07 +0800

    x

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,45 @@
-use std::{fs::File, io::Write, sync::Arc};
+use std::sync::Arc;
 
 use anyhow::Result;
 use ethers::{
     providers::{Http, Middleware, Provider, ProviderExt},
-    types::{Address, Block, Transaction},
-    utils::hex::ToHex,
+    types::Address,
+    utils::{hex::ToHex, parse_ether},
 };
-use tokio::task::JoinHandle;
+use tokio::sync::Semaphore;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     //连接https rpc
     let client = Arc::new(Provider::<Http>::connect("http://localhost:8545").await);
 
-    let from_block = 19272723_u64;
+    let from_block = 19394000_u64;
 
-    // let leatest_block = 19272733_u64;
     let leatest_block = client.get_block_number().await?.as_u64();
 
-    let output_file = "output.txt";
+    let value = parse_ether("25").unwrap();
 
-    let mut file = std::fs::File::create(output_file)?;
-
+    let semaphore = Arc::new(Semaphore::new(100));
     let mut tasks = Vec::new();
 
     for i in from_block..leatest_block {
+        let semaphore = semaphore.clone();
         let client = client.clone();
         tasks.push(tokio::spawn(async move {
-            let block = client.get_block_with_txs(i).await?;
-            match block {
-                Some(block) => Ok(block),
-                None => Err(anyhow::anyhow!("block not found")),
+            let _permit = semaphore.acquire().await.unwrap();
+            let block = client.get_block_with_txs(i).await.unwrap();
+            let txs = block.unwrap_or_default().transactions;
+            for tx in txs {
+                if tx.value == value {
+                    println!("tx: {}", tx.hash().to_string());
+                }
             }
         }));
-
-        if tasks.len() >= 50 {
-            process_joins(&mut file, &mut tasks).await?;
-            tasks.clear();
-        }
-
-        println!("block: {}", i);
     }
 
-    process_joins(&mut file, &mut tasks).await?;
+    futures::future::join_all(tasks).await;
     println!("done");
     Ok(())
-}
-
-async fn process_joins(
-    file: &mut File,
-    joins: &mut Vec<JoinHandle<Result<Block<Transaction>>>>,
-) -> Result<()> {
-    for join in joins {
-        let block = join.await??;
-        process_block_with_txs(file, block).await;
-    }
-
-    Ok(())
-}
-
-async fn process_block_with_txs(file: &mut File, block: Block<Transaction>) {
-    let mut froms = vec![];
-    for tx in block.transactions {
-        froms.push(tx.from);
-    }
-    froms.sort();
-    froms.dedup();
-    // file 追加写入
-
-    for from in froms {
-        file.write(format!("{}\n", fmt_address(from)).as_bytes())
-            .unwrap();
-    }
 }
 
 pub fn fmt_address(address: Address) -> String {
